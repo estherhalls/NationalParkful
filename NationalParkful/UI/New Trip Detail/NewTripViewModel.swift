ca8b9aa260d6785faea0ccf5b87c6802132e75f2
Esther Halls <esthernhalls@gmail.com>
2022-12-01 00:44:29 -0700

    view models, controllers created for each storyboard and subclassed views

---
+++ NationalParkful/UI/New Trip Detail/NewTripViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewTripViewModel.swift
+//  NationalParkful
+//
+//  Created by Esther on 12/1/22.
+//
+
+import Foundation
---
+++ NationalParkful/UI/ParkDetail-ReadOnly/ParkDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ParkDetailViewModel.swift
+//  NationalParkful
+//
+//  Created by Esther on 12/1/22.
+//
+
+import Foundation
