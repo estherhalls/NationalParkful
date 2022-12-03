f137ba9b59d58903d9fce2c9280f5446da31e154
Esther Halls <esthernhalls@gmail.com>
2022-12-02 21:57:57 -0700

    home page constraint refinement

--- NationalParkful/UI/Home Settings/HomeViewController.swift
+++ NationalParkful/UI/Home Settings/HomeViewController.swift
@@ -12,7 +12,9 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
+        
+        
     }
     
     
