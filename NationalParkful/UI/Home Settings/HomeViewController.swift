a1d9cc866886ea4fc75c5261ead9add128143668
Esther Halls <esthernhalls@gmail.com>
2022-12-02 17:55:20 -0700

    wired up home buttons and ironed out navigation, still having issue with nav bars stacking when segue from home rather than tab bar

--- NationalParkful/UI/Home Settings/HomeViewController.swift
+++ NationalParkful/UI/Home Settings/HomeViewController.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
     // MARK: - Navigation
 
